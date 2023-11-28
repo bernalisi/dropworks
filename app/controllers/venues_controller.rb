7b50818cd79f719b8fab89851da7d4d1de5b23ea
Anna Subotenko <anna.subotenko.uk@gmail.com>
2023-11-28 14:49:24 +0100

    Set up a basic map with cards

--- app/controllers/venues_controller.rb
+++ app/controllers/venues_controller.rb
@@ -3,6 +3,13 @@ class VenuesController < ApplicationController
 
   def index
     @venues = Venue.all
+    @markers = @venues.geocoded.map do |venue|
+      {
+        lat: venue.latitude,
+        lng: venue.longitude,
+        info_window_html: render_to_string(partial: "info_window", locals: { venue: venue })
+      }
+    end
   end
 
   def show
