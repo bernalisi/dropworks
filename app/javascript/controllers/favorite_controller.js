aed3add609d62e871004e85decd12242e9f64219
Giorgio Mazzuca <mazzucagiorgio@gmail.com>
2023-11-29 09:57:15 +0100

    first try stimulus favorite

---
+++ app/javascript/controllers/favorite_controller.js
@@ -0,0 +1,21 @@
+// app/javascript/controllers/favorite_controller.js
+import { Controller } from "@hotwired/stimulus";
+
+export default class extends Controller {
+  // Connect this controller to a "favorite" button
+  static targets = ["heartIcon"];
+
+  connect() {
+    // Your favorite logic goes here
+    console.log("Item favorited!");
+  }
+
+  // The action to be called when the "favorite" button is clicked
+  favorite(event) {
+    event.preventDefault();
+
+    // Toggle the heart icon class based on the favorite status
+    this.heartIconTarget.classList.toggle("fa-regular", !this.element.dataset.favorite);
+    this.heartIconTarget.classList.toggle("fa-solid", this.element.dataset.favorite);
+  }
+}
