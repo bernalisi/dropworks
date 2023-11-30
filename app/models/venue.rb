18e564e79302d0b1b7ba3a598764e87bbe9110e2
BERNARDO LISI <bernardo.lisi.work@gmail.com>
2023-11-30 12:22:11 +0100

    added pg search gem, modified venue model and controller, created a search html and css partials and added a render script in the venue index page

--- app/models/venue.rb
+++ app/models/venue.rb
@@ -1,16 +1,26 @@
 class Venue < ApplicationRecord
-  belongs_to :user
 
+  #database relationships
+  belongs_to :user
   has_many_attached :photos
   has_many :reviews, through: :bookings
   has_many :bookings, dependent: :destroy
   has_many  :favorites, dependent: :destroy
   has_many  :opening_hours, dependent: :destroy
 
+  #input validations for new instances
   validates :name, presence: true
   validates :address, presence: true
 
+  #mapbox scripts
   geocoded_by :address
-
   after_validation :geocode, if: :will_save_change_to_address?
+
+  #pg_search scripts
+  include PgSearch::Model
+  pg_search_scope :search_by_name_and_address,
+  against: [ :name, :address ],
+  using: {
+    tsearch: { prefix: true }
+  }
 end
