c2f1b1944146664217e48c09e1382a8207d92809
Giorgio Mazzuca <mazzucagiorgio@gmail.com>
2023-11-30 13:04:53 +0100

    changed button after made a review

--- app/models/venue.rb
+++ app/models/venue.rb
@@ -2,7 +2,7 @@ class Venue < ApplicationRecord
   belongs_to :user
 
   has_many_attached :photos
-  has_many :reviews, through: :bookings
+#  has_many :reviews, through: :bookings
   has_many :bookings, dependent: :destroy
   has_many  :favorites, dependent: :destroy
   has_many  :opening_hours, dependent: :destroy
