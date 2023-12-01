8f2930f08bd730e3cc3b81d7f699b6b495304bfb
Giorgio Mazzuca <mazzucagiorgio@gmail.com>
2023-12-01 16:31:08 +0100

    working o the show page

--- config/importmap.rb
+++ config/importmap.rb
@@ -10,3 +10,4 @@ pin "@popperjs/core", to: "popper.js", preload: true
 pin "mapbox-gl", to: "https://ga.jspm.io/npm:mapbox-gl@2.15.0/dist/mapbox-gl.js"
 pin "process", to: "https://ga.jspm.io/npm:@jspm/core@2.0.1/nodelibs/browser/process-production.js"
 pin "flatpickr", to: "https://ga.jspm.io/npm:flatpickr@4.6.13/dist/esm/index.js"
+pin "@splidejs/splide", to: "https://ga.jspm.io/npm:@splidejs/splide@4.1.4/dist/js/splide.esm.js"
