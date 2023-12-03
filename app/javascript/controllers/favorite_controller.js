c0af98ce9caca4af8c5b4f90bedd8a1c623da8bc
Giorgio Mazzuca <mazzucagiorgio@gmail.com>
2023-12-03 13:57:45 +0100

    changed link to div icons

--- app/javascript/controllers/favorite_controller.js
+++ app/javascript/controllers/favorite_controller.js
@@ -2,15 +2,36 @@
 import { Controller } from "@hotwired/stimulus";
 
 export default class extends Controller {
-  static targets = ["heartIcon", "saveText];
+  static targets = ["heartIcon", "saveText", "reviewButton"];
 
   connect() {
-    console.log("Item favorited!");
+    this.checkReviewStatus();
   }
+
+  checkReviewStatus() {
+    const heartIcon = this.heartIconTarget;
+    const reviewButton = this.reviewButtonTarget;
+
+    // Check if the Save text content is "Saved"
+    const reviewEnabled = heartIcon.classList.contains("bi-bookmark-heart-fill");
+    console.log(reviewButton);
+    // Optionally, update the review button state based on the reviewEnabled status
+    if (reviewEnabled) {
+      reviewButton.style.pointerEvents = "auto"; // Enable click events
+      reviewButton.style.opacity = "1"; // Optionally, reset opacity
+    } else {
+      reviewButton.style.pointerEvents = "none"; // Disable click events
+      reviewButton.style.opacity = "0.5"; // Optionally, reduce opacity to visually indicate it's disabled
+    }
+  }
+
+
+
 
   favorite() {
     const heartIcon = this.heartIconTarget;
     const saveText = this.saveTextTarget;
+
 
     // Toggle the heart icon class based on the favorite status
     heartIcon.classList.toggle("bi-bookmark-heart-fill");
@@ -22,5 +43,7 @@ export default class extends Controller {
     } else {
       saveText.textContent = "Save";
     }
+    // Update the review button status after changing the Save text content
+    this.checkReviewStatus();
   }
 }
