f9e66864e61a4bf785ca39811e5cdeef8a684492
Anna Subotenko <anna.subotenko.uk@gmail.com>
2023-12-05 15:58:57 +0100

    camera added

--- app/javascript/controllers/camera_controller.js
+++ app/javascript/controllers/camera_controller.js
@@ -4,41 +4,39 @@ import QrScanner from 'qr-scanner';
 // Connects to data-controller="camera"
 export default class extends Controller {
 
-  static targets = ["input", "parag"]
+  static targets = ["reader", "result"]
 
   connect() {
-    this.scanned = false
+    console.log(this.resultTarget)
+    setTimeout(() => {
+
+      this.scanned = false
 
     // console.log(this.element)
     this.qrScanner = new QrScanner(
-      this.inputTarget,
+      this.readerTarget,
       result =>
         this.createTag(result), {
       highlightCodeOutline: true
       }
     )
 
+    qrScanner.setCamera(facingModeOrDeviceId);
+
     this.qrScanner.start();
+    console.log(this.qrScanner)
+
+    }, 500);
+
     }
 
     createTag(url) {
       url = "https://dropworks-ae9dcc6994cc.herokuapp.com/index"
       const atag = `<a href=${url}> Confirm </a>`
-        this.inputTarget.insertAdjacentHTML("beforeend", atag)
+        this.readerTarget.insertAdjacentHTML("beforeend", atag)
 
         this.qrScanner.stop();
     }
 
-  openCamera(e) {
 
-    const allButtons = document.querySelectorAll('.navbar-icons');
-    allButtons.forEach(button => {
-      button.querySelector('i').classList.remove('active-item');
-      button.querySelector('p').classList.remove('active-item');
-    });
-
-    this.inputTarget.click()
-    e.currentTarget.classList.add("active-item")
-    this.paragTarget.classList.add("active-item")
-  }
 }
